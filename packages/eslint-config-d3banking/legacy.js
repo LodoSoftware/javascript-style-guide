2841ff14df40004a5788bb9131277f2a8bad2aac
Jacob Parish <jacob.parish.1@gmail.com>
2017-05-22 09:43:30 -0500

    feat: Remove legacy rules and start migrating temporary warnings to errors.

--- packages/eslint-config-d3banking/legacy.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: [
-    'eslint-config-airbnb-base/legacy',
-    './rules/imports',
-    './rules/d3overrides',
-    './rules/d3-es5'
-  ].map(require.resolve),
-  rules: {}
-};
