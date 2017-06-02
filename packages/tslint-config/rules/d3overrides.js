9b769dd5d44b00a52505eeca3e7b05c71a302a57
Jacob Parish <jacob.parish.1@gmail.com>
2017-06-02 10:47:24 -0500

    feat: warnings for max-line-length and type-def

--- packages/tslint-config/rules/d3overrides.js
+++ packages/tslint-config/rules/d3overrides.js
@@ -1,19 +1,41 @@
 module.exports = {
   rules: {
+    // Errors
     'quotemark': [true, 'single'],
-    'member-access': false,
-    'trailing-comma': [true, { 'multiline': 'never', 'singleline': 'never' }],
-    'ordered-imports': [false],
-    'object-literal-sort-keys': false,
-    'member-ordering': [false],
+    'trailing-comma': [true, {
+      'multiline': 'never',
+      'singleline': 'never'
+    }],
     'variable-name': [true,
       'ban-keywords',
       'check-format',
       'allow-pascal-case',
       'allow-leading-underscore'
     ],
+
+    // Warnings
+    'max-line-length': {
+      'severity': 'warn',
+      'options': 200
+    },
+    'typedef': {
+      'severity': 'warn',
+      'options': [
+        'call-signature',
+        'parameter',
+        'arrow-parameter',
+        'property-declaration',
+        'variable-declaration',
+        'member-variable-declaration'
+      ]
+    },
+
+    // Excludes
+    'member-access': false,
+    'ordered-imports': [false],
+    'object-literal-sort-keys': false,
+    'member-ordering': [false],
     'interface-name': [false],
-    'max-line-length': [true, 200],  // Same as our eslint-config
     'interface-over-type-literal': false
   }
 }
