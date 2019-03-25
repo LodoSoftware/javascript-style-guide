712dd4c66656af29006c2bc307f866f46ace25f6
Jacob Parish <jparish@d3banking.com>
2019-03-25 09:05:17 -0500

    fix: exclude react rules that prettier handles

--- packages/eslint-config/rules/prettier.js
+++ packages/eslint-config/rules/prettier.js
@@ -3,7 +3,8 @@ const path = require('path');
 module.exports = {
   extends: [
     path.resolve(require.resolve('eslint-config-prettier')),
-    path.resolve(require.resolve('eslint-config-prettier/@typescript-eslint'))
+    path.resolve(require.resolve('eslint-config-prettier/@typescript-eslint')),
+    path.resolve(require.resolve('eslint-config-prettier/react'))
   ],
   plugins: ['prettier'],
   rules: {
