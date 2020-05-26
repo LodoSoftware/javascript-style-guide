c97bcca6d84f099f7ab9306cf7708c45ec271033
Jacob Parish <jacob.parish.1@gmail.com>
2020-05-26 12:31:09 -0500

    feat: Added testing-library rules

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -3,6 +3,7 @@ module.exports = {
     'eslint-config-airbnb',
     './rules/imports',
     './rules/react',
+    './rules/testing-library',
     './rules/d3overrides'
   ].map(require.resolve),
   rules: {}
