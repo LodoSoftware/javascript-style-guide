a026dfd73d5e0fa62240da85ca6a00f9ad3b59a2
Jason Stratman <Jason.Stratman@ncr.com>
2021-02-03 08:56:04 -0600

    Revert "feat(linting): add naming-convention enum case and boolean var name rules"
    This reverts commit 39f21a415f988a9a01d18f872c6af11d786dea8a.

--- packages/eslint-config/rules/typescript.js
+++ packages/eslint-config/rules/typescript.js
@@ -67,20 +67,6 @@ module.exports = {
       }
     ],
 
-    // Only allow boolean variables with appropriate prefixes
-    //  - PascalCase needed for reasons, it is really camelCase: https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/naming-convention.md#enforce-that-boolean-variables-are-prefixed-with-an-allowed-verb
-    // Only allow PascalCase for enum members
-    '@typescript-eslint/naming-convention': [
-      'warn',
-      {
-        selector: 'variable',
-        types: ['boolean'],
-        format: ['PascalCase'],
-        prefix: ['is', 'should', 'has', 'can', 'did', 'will']
-      },
-      { selector: 'enumMember', format: ['PascalCase'] }
-    ],
-
     // Use typescript version of this rule
     'no-shadow': 'off',
     '@typescript-eslint/no-shadow': 'error',
