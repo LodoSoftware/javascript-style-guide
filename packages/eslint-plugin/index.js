5103893cbff28be4e969eff3a4764643838e551e
jrparish <jacob.parish.1@gmail.com>
2021-06-07 12:09:21 -0500

    fix: Fixed no-use-dispatch rule import

--- packages/eslint-plugin/index.js
+++ packages/eslint-plugin/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   rules: {
     'no-use-selector': require('./rules/no-use-selector'),
-    'no-use-dispatch': require('./rules/no-use-selector')
+    'no-use-dispatch': require('./rules/no-use-dispatch')
   }
 };
