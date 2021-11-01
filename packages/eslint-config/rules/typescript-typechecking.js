abd17cf282f37da385d7a30a6cab8b5639d31ba8
jrparish <jacob.parish.1@gmail.com>
2021-11-01 09:41:17 -0500

    fix: changed @typescript-eslint/no-unsafe-argument to warning

--- packages/eslint-config/rules/typescript-typechecking.js
+++ packages/eslint-config/rules/typescript-typechecking.js
@@ -20,6 +20,7 @@ module.exports = {
     '@typescript-eslint/no-unsafe-return': 'warn',
     '@typescript-eslint/no-unsafe-call': 'warn',
     '@typescript-eslint/no-unsafe-member-access': 'warn',
+    '@typescript-eslint/no-unsafe-argument': 'warn',
 
     // Helps to catch unhandled nullability in string templates
     '@typescript-eslint/restrict-template-expressions': 'warn',
